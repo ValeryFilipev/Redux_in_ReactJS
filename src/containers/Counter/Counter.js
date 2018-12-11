3371fcbcb6d80ae87505ebda17b1cd18b8bb4598
Valery Filipyev <valeranuzhdin1998@mail.ru>
2018-12-11 18:32:50 +0300

    install Redux

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -6,7 +6,7 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 class Counter extends Component {
     state = {
         counter: 0
-    }
+    };
 
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
