abb2bdda3080ba9b5668b92f8d7b080a4a8a6700
Валерий <valeranuzhdin1998@mail.ru>
2018-12-11 23:06:50 +0300

    create logic of redux

--- redux-basics.js
+++ redux-basics.js
@@ -7,12 +7,29 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    }
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
   return state;
 };
 
 // Store
 const store = createStore(rootReducer);
 
-// Dispatching Action
-
 // Subscription
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
